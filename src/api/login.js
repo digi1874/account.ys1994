dfd1f7f17a70d5d21c2d43a710098c36ba39453a
林振辉 <315713565@163.com>
2020-03-17 17:19:00 +0800

    登出

--- src/api/login.js
+++ src/api/login.js
@@ -2,9 +2,11 @@
  * @Author: lin.zhenhui
  * @Date: 2020-03-06 23:20:28
  * @Last Modified by: lin.zhenhui
- * @Last Modified time: 2020-03-16 21:40:40
+ * @Last Modified time: 2020-03-17 17:11:40
  */
 
-import { request } from '@/utils'
+import { request, getToken } from '@/utils'
 
-export const logout = () => request.post(process.env.NODE_ENV === 'development' ? 'http://localhost:8089/login/logout' : '/login/logout')
+const logoutHref = process.env.NODE_ENV === 'development' ? 'http://localhost:8089/login/logout/' : '/login/logout/'
+
+export const logout = () => request.post(logoutHref + getToken())
